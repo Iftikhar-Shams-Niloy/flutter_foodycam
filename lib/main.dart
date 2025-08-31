3926d20fa4febdf2892bfac30420f2f771495ac1
Iftikhar-Shams-Niloy <niloyshams21@gmail.com>
2025-08-31 10:59:56 +0600

    App roation has been fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'presentation/pages/home_screen.dart';
 import 'core/constants.dart';
 
 void main() {
-  runApp(const FoodClassifierApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then((
+    func,
+  ) {
+    runApp(const FoodClassifierApp());
+  });
 }
 
 class FoodClassifierApp extends StatelessWidget {
   const FoodClassifierApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
